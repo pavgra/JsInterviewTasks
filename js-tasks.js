545f5c3ae43eba00d3fb7707f4f8d3299d0eb4a9
Pavel Grafkin <pavel.grafkin@firstlinesoftware.com>
2016-10-27 13:04:44 +0300

    Added some initial tasks

--- js-tasks.js
+++ js-tasks.js
@@ -1,18 +1,28 @@
-function passVar(obj1, obj2, num) {
-    obj1.prop = "laptop"; // will CHANGE original
-    obj2 = { prop: "computer" }; //will NOT affect original
-    num = num + 1; // will NOT affect original
+/**
+ * Pass vakue by value vs pass reference by value.
+ */
+
+function changeStuff(a, b, c)
+{
+  a = a * 10;
+  b.item = "changed";
+  c = {item: "changed"};
 }
 
-var object1 = {
-    prop: "car"
-};
-var object2 = {
-    prop: "bike"
-};
-var number1 = 10;
+var num = 10;
+var obj1 = {item: "unchanged"};
+var obj2 = {item: "unchanged"};
 
-passVar(object1, object2, number1);
-console.log(object1); //output: Object {item:"laptop"}
-console.log(object2); //output: Object {item:"bike"}
-console.log(number1); //ouput: 10
+changeStuff(num, obj1, obj2);
+
+console.log(num);
+console.log(obj1.item);
+console.log(obj2.item);
+
+/**
+ * Output:
+ * 
+ * 10
+ * changed
+ * unchanged
+*/
